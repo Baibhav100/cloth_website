2e794e47cff9856d48a21aea99c48b1ee5365be7
Baibhav Rajkumar <96993916+Baibhav100@users.noreply.github.com>
2025-01-20 23:16:01 -0800

    new changes done in the footer

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div>
         <Grid2 classname='bg-black text-white text-center mt-10' sx={{bgcolor:"black",color:"white",py:3}}>
-            
+            This is a footer>
 
         </Grid2>
     </div>
